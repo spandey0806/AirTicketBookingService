53aff907d1a58ae780e1b73b9372a43b32418582
Shubham Pandey <shubhampandey0806@gmail.com>
2023-09-29 15:06:13 +0530

    completed created booking

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -1,3 +1,5 @@
+const { StatusCodes } = require('http-status-codes');
+
 const { Booking } = require ('../models/index');
 const { AppError , ValidationError } = require('../utils/errors/index');
  
@@ -5,12 +7,35 @@ const { AppError , ValidationError } = require('../utils/errors/index');
 class BookingRepository {
     async create (data){
         try {
-            
+            const booking = await Booking.create(data)
+            return booking;
         } catch (error) {
             if(error.name === 'SequelizeValidationError'){
                 throw new ValidationError(error);
             }
-            throw new AppError;
+            throw new AppError(
+                'RepositoryError',
+                'Cannot create Booking' ,
+                'There was some issue creating Booking,please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR
+                );
+        }
+    }
+
+    async update(bookingId, data) {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if(data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError', 
+                'Cannot update Booking', 
+                'There was some issue updating the booking, please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR);
         }
     }
 
--- src/utils/errors/validation-error.js
+++ src/utils/errors/validation-error.js
@@ -1,15 +1,16 @@
  const { StatusCodes } = require('http-status-codes')
 
 class ValidationError extends Error {
-    constructor (
-        
-        explanation = [],
-        statusCode = StatusCodes.BAD_REQUEST
-    ){
+    constructor(error) {
+        super();
+        let explanation =[];                             // sequelize return errors in this form so 
+        error.errors.forEach((err)=>{                   //message properties extracted from the error.errors array
+            explanation.push(err.message);    
+        })
         this.name = 'ValidationError';
         this.message='Not able to validate the data sent in the request';
         this.explanation=explanation;
-        this.statusCode=statusCode;
+        this.statusCode=StatusCodes.BAD_REQUEST;
     }
 }
 
